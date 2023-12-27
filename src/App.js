193ae1ee4d124ba4c78e08c3637cb16e8c12c662
THE LUJAIN <silversocer17@gmail.com>
2023-12-27 20:35:53 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
-
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Field, Form, Formik } from "formik";
+import { useState } from "react";
 function App() {
+  const [userInfo, setUserInfo] = useState(5);
+  let x = 1;
+  console.log(userInfo);
+  // function handleValues(values) {
+  //   console.log(values);
+  // }
+  // const init = {
+  //   username: "",
+  //   password: "",
+  // };
   return (
-    <div id="page-top">
-      <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+    <div>
+      {/* <Formik initialValues={init} onSubmit={handleValues}>
+        <Form>
+          <div>
+            <Field name="username" />
+          </div>
+          <div>
+            <Field name="password" />
+          </div>
+          <button>login</button>
+        </Form>
+      </Formik> */}
+
+      <button onClick={() => setUserInfo(userInfo + 1)}>+</button>
+      <div>{userInfo}</div>
+      <button onClick={() => setUserInfo(userInfo - 1)}>-</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// const CustomInput = (props) => <input className="custom-input" {...props} />;
+//  const CustomTextarea = (props) => <textarea className="custom-text-area" {...props} />;
+
+//  const App = () => (
+//    <Page>
+//      <FormiumForm
+//        slug="support-request"
+//        components={{
+//          TextInput: CustomInput,
+//          Textarea: CustomTextarea
+//        }}
+//      />
+//    </Page>
+//  );
+
+//  import React from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// function App() {
+//  return (
+//    <div className="App">
+//      <center>
+//        <h1>Register a new account</h1>
+//        <Formik>
+
+//            <Form>
+//              <Field
+//                type="text"
+//                name="fullname"
+//                placeholder="Enter your fullname"
+//              />
+//              <ErrorMessage name="fullname" component="div" />
+
+//              <Field
+//                type="email"
+//                name="email"
+//                placeholder="Enter email address"
+//              />
+//              <ErrorMessage name="email" component="div" />
+
+//              <Field type="password" name="password" />
+//              <ErrorMessage name="password" component="div" />
+
+//              <button type="submit" disabled={isSubmitting}>
+//                Submit
+//              </button>
+//            </Form>
+//          )}
+//        </Formik>
+//      </center>
+//    </div>
+//  );
+// }
+// export default App;
